89b4f190875c886b6c6eba8690d418718f89f1a9
Brandon Antonelli <bantonelli07@gmail.com>
2015-04-23 03:51:31 -0400

    Set up new version of Payment view for api_v1: 	- renamed payment view to KitBuilderPaymentView 	- Changed wording on email subject 	- Changed zip file directory to be "kitbuilder_purchases/<user_id>/<filename>.zip"

--- api/api_v1/urls.py
+++ api/api_v1/urls.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.urlpatterns import format_suffix_patterns
 from .kitbuilder.views import *
 from .userprofile.views import *
+import views
 
 urlpatterns = patterns('',
                         url(r'^login/', include('rest_framework.urls', namespace='rest_framework')),
@@ -18,8 +19,7 @@ urlpatterns = patterns('',
                         # # url(r'^api/samples/endproducts/(?P<pk>[0-9]+)/$', views.SampleDetail.as_view()),
                         # url(r'^custom-kits/$', views.CustomKitList.as_view()),
                         # url(r'^custom-kits/(?P<pk>[0-9]+)/$', views.CustomKitDetail.as_view()),
-                        # # purchase view requires csrf token.
-                        # #url(r'^api/custom-kits/purchase/$', csrf_exempt(views.CustomKitPaymentView.as_view())),
-                        # url(r'^custom-kits/purchase/$', views.CustomKitPaymentView.as_view()),
+                        # purchase view requires csrf token.
+                        url(r'^custom-kits/purchase/$', views.KitBuilderPaymentView.as_view()),
                        )
 
