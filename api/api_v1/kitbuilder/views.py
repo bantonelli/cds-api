f41fdfa76abd05a74b8a35bf27e43874d1598afb
Brandon Antonelli <bantonelli07@gmail.com>
2015-04-23 21:01:31 -0400

    Start working on API_V1 kitbuilder views.

--- api/api_v1/kitbuilder/views.py
+++ api/api_v1/kitbuilder/views.py
@@ -1,69 +1,96 @@
-# from rest_framework import generics, permissions
-# from api.permissions import IsKitOwner, IsUser
-# from serializers import *
-# from provider.oauth2.models import AccessToken
-# from kitbuilder.kitbuilder_beta.models import Sale, Tag, KitDescription, Kit, Sample, CustomKit
-# from userprofile.models import UserProfile
-# from django.conf import settings
-# from django.core.mail import send_mail, EmailMessage
-# from django.core.exceptions import ObjectDoesNotExist
-#
-# User = get_user_model()
-#
-#
-# ########### API VIEWS
-#
-#
-# #SOUND SAMPLE
-# class SampleDemoList(generics.ListAPIView):
-#     permission_classes = (permissions.IsAuthenticated,)
-#     required_scopes = ['read']
-#     queryset = Sample.objects.all()
-#     serializer_class = SampleDemoSerializer
-#
-#
-# class SampleDemoDetail(generics.RetrieveAPIView):
-#     permission_classes = (permissions.IsAuthenticated,)
-#     required_scopes = ['read']
-#     queryset = Sample.objects.all()
-#     serializer_class = SampleDemoSerializer
-#
-#
-# #SAMPLE KIT
-# class KitList(generics.ListAPIView):
-#     """
-#     List all Drum Kits
-#     """
-#     permission_classes = (permissions.IsAuthenticated, )
-#     required_scopes = ['read']
-#     queryset = Kit.objects.all()
-#     serializer_class = KitSerializer
-#
-#
-# class KitDetail(generics.RetrieveAPIView):
+from django.conf import settings
+from django.core.mail import send_mail, EmailMessage
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from api.permissions import IsKitOwner, IsUser
+from serializers import *
+from kitbuilder.kitbuilder_v1.models import Sale, Tag, Vendor, VendorKit, Sample, KitBuilderPurchase, KitBuilderTemplate
+from userprofile.models import UserProfile
+from djoser.utils import ActionViewMixin, SendEmailViewMixin
+from djoser.views import OauthUserMixin
+
+
+
+########### API VIEWS
+
+
+#SOUND SAMPLE
+class SamplePreviewList(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    required_scopes = ['read']
+    queryset = Sample.objects.all()
+    serializer_class = SamplePreviewSerializer
+
+
+class SamplePreviewDetail(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    required_scopes = ['read']
+    queryset = Sample.objects.all()
+    serializer_class = SamplePreviewSerializer
+
+
+#SAMPLE KIT
+class VendorKitList(generics.ListAPIView):
+    """
+    List all Drum Kits
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    required_scopes = ['read']
+    queryset = VendorKit.objects.all()
+    serializer_class = VendorKitSerializer
+
+
+class VendorKitDetail(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAdminUser, )
+    queryset = VendorKit.objects.all()
+    serializer_class = VendorKitSerializer
+
+
+#-------------------------------------------------------------->
+# KIT BUILDER PURCHASE
+# class KitBuilderPurchaseList(generics.ListCreateAPIView):
 #     permission_classes = (permissions.IsAdminUser, )
-#     queryset = Kit.objects.all()
-#     serializer_class = KitSerializer
-#
-#
-# #KIT DESCRIPTIONS (USE FOR QUICK TESTING)
-# class KitDescriptionList(generics.ListAPIView):
-#     permission_classes = (permissions.IsAdminUser, )
-#     required_scopes = ['read']
-#     queryset = KitDescription.objects.all()
-#     serializer_class = KitDescriptionSerializer
-#
-#
-# #CUSTOM KIT
-# class CustomKitList(generics.ListCreateAPIView):
-#     permission_classes = (permissions.IsAdminUser, )
-#     queryset = CustomKit.objects.all()
-#     serializer_class = CustomKitSerializer
-#
-#
-# class CustomKitDetail(generics.RetrieveDestroyAPIView):
-#     # Make Custom permission so that it checks if is authenticated and is user that created kit
-#     permission_classes = (permissions.IsAuthenticated, IsKitOwner, )
-#     required_scopes = ['read']
-#     queryset = CustomKit.objects.all()
-#     serializer_class = CustomKitPurchasedSerializer
+#     queryset = KitBuilderPurchase.objects.all()
+#     serializer_class = KitBuilderPurchaseSerializer
+
+
+class KitBuilderPurchaseDetail(generics.RetrieveDestroyAPIView):
+    # Make Custom permission so that it checks if is authenticated and is user that purchased kit
+    permission_classes = (permissions.IsAuthenticated, IsKitOwner, )
+    required_scopes = ['read']
+    queryset = KitBuilderPurchase.objects.all()
+    serializer_class = KitBuilderPurchaseSerializer
+
+
+class KitBuilderTemplateList(generics.ListCreateAPIView, OauthUserMixin):
+    permission_classes = (permissions.IsAuthenticated, )
+    required_scopes = ['read']
+    queryset = KitBuilderTemplate.objects.all()
+    serializer_class = KitBuilderTemplateSerializer
+
+    def create(self, request, *args, **kwargs):
+        # data = request.DATA
+        # user = User(
+        #     profit_and_loss=data['component_comments'],
+        #     name=data['name']
+        # )
+        # user.clean()
+        # user.save()
+        #
+        # UserProfile.objects.create(
+        #     user=user,
+        #     name=data['profile']['name']
+        # )
+        #
+        # serializer = UserSerializer(user)
+        # headers = self.get_success_headers(serializer.data)
+        data = {"Template Created": True}
+        return Response(data=data, status=status.HTTP_201_CREATED)
+
+
+class KitBuilderTemplateDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated, IsKitOwner, )
+    required_scopes = ['read']
+    queryset = KitBuilderTemplate.objects.all()
+    serializer_class = KitBuilderTemplateSerializer
