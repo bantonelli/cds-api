d16f35b186c7765b50578fd08a466069b80f2f82
Brandon Antonelli <bantonelli07@gmail.com>
2015-05-13 20:54:53 -0400

    Add more requests to DRF throttle.

--- CDSAPI/settings.py
+++ CDSAPI/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        'user': '1000/day',
+        'user': '10000/day',
     },
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
