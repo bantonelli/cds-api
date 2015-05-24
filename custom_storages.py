f9f39e816508b5bfd14aab3e1c72548db05ad105
Brandon Antonelli <bantonelli07@gmail.com>
2015-05-24 02:56:40 -0400

    - Override the 'connection' property and pass in the host parameter to connection constructor.

--- custom_storages.py
+++ custom_storages.py
@@ -8,6 +8,15 @@ from storages.backends.s3boto import S3BotoStorage
 class StaticStorage(OptimizedCachedStaticFilesStorage):
     location = STATICFILES_LOCATION
 
+    # Had to override this property to pass in the 'host' parameter and quiet error
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self.connection_class(
+                self.access_key, self.secret_key, host='s3.amazonaws.com',
+                calling_format=self.calling_format)
+        return self._connection
+
 
 class MediaStorage(S3BotoStorage):
     location = MEDIAFILES_LOCATION
